5fd3939db2261308150dceab07b505831f185f7a
Fábio Pereira <fabiopereira.fps@gmail.com>
2021-04-04 01:29:35 -0300

    Add validation

--- submissions/controllers/requests/add_submission.go
+++ submissions/controllers/requests/add_submission.go
@@ -1,8 +1,8 @@
 package requests
 
 type AddSubmissionRequest struct {
-	Content     string
-	ActivityId  uint
-	ClassroomId uint
-	StudentId   uint
+	Content     string `json:"content" binding:"required"`
+	ActivityId  uint   `json:"activity_id" binding:"required"`
+	ClassroomId uint   `json:"classroom_id" binding:"required"`
+	StudentId   uint   `json:"student_id" binding:"required"`
 }
